42e0e675026727eac18eccb794cf34c25c6fcd38
luzza07 <luzzadangol456@gmail.com>
2024-07-09 07:14:36 +0545

    first qa

--- tests/UGLogin/uglogin.spec.js
+++ tests/UGLogin/uglogin.spec.js
@@ -13,7 +13,8 @@ test('has title', async ({ page }) => {
     await expect(page).toHaveTitle('UG Cakes | Happiness At Your Doorsteps| Online Cakes in Kathmandu | Send Cakes To Nepal');
 });
 
-test('Login and add to cart', async ({ page }) => {
+test.describe("UG Cakes successful Login",()=>{
+test('Login with valid user', async ({ page }) => {
     // Click on the "Login" link
     await page.click('a:has-text("Login")');
   
@@ -26,16 +27,18 @@ test('Login and add to cart', async ({ page }) => {
     await page.locator('//*[@id="login_password"]').fill("Nicky9880367728");
     await page.locator('//*[@id="root"]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/form/div[3]/button/span[1]').click();
     await expect(page.locator('//*[@id="menu-nav"]/li/div/span/span')).toHaveText("Shop By Categories");
+});
 
+test('Open  individual item'),async({page}) =>{
     await page.locator('//*[@id="root"]/div/div[2]/div[2]/div/div/div[1]/a/div/div/span[2]').click();
     await expect(page.locator('//*[@id="root"]/div/div[2]/div[1]/div/div/h1')).toHaveText("Dry Cakes");
-
     await page.locator('//*[@id="root"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div[2]/div/div[2]/button[2]').click();
     await page.evaluate(() => {
         window.scrollTo(0, 0); // Scroll to the top
       });
     await page.locator('/html/body/div[7]/div/div/div/div[2]/div/div/div/div[2]/a/button/span').click();
     await expect(page.locator('//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div[1]/div/h1')).toHaveText("My Cart");
+}
 });
 
 // test('Valid Email Address Login test', async ({ page }) => {
